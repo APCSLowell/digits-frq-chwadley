36e6cba496d07977a600c23edf6b53342ef95970
chwadley <chwadley@s.sfusd.edu>
2025-04-10 10:34:56 -0700

    first try

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -1,11 +1,9 @@
 import java.util.ArrayList;
 
-public class Digits
-{
-
+public class Digits {
 	private ArrayList<Integer> digitList;
 
-	public Digits(int num) {
+	public Digits (int num) {
 		if (num==0) digitList.add(0);
 		while (num>0) {
 			digitList.insert(0,num%10);
@@ -13,14 +11,14 @@ public class Digits
 		}
 	}
 
-	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
-
+	public boolean isStrictlyIncreasing() {
+		for (int i=0;i<digitList.size()-1;i++) {
+			if (digitList.get(i)>digitList.get(i+1)) return false;
+		}
+		return true;
 	}
 	
-	public String toString()
-	{
+	public String toString() {
 		return digitList.toString();
 	}
 }
